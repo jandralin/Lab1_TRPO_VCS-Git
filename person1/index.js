c93eaed9d069046bb3077a5b6ceb6084643cdfb0
Anastasia Vasyukova <a.vas02@bk.ru>
2024-09-10 22:38:16 +0300

    Добавлены три файла

---
+++ person1/index.js
@@ -0,0 +1,21 @@
+
+const fileInput = document.getElementById('fileInput');
+const fileContent = document.getElementById('fileContent');
+const loader = document.getElementById('text-contain')
+
+fileInput.addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    
+    if (file && file.type === 'text/plain') {
+        const reader = new FileReader();
+       
+        reader.readAsText(file);
+				reader.onload = function(e) {
+					fileContent.textContent = e.target.result;
+					loader.classList.add('text-contain')
+			};
+    } else {
+        fileContent.textContent = 'Please select a valid text file.';
+    }
+});
+
